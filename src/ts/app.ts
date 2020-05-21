f78fdd82b98aff533158ae7c060723a847cfdab2
fernanedez1992Mike <fernandez1992mike@gmail.com>
2020-05-21 14:58:37 -0400

    First Commit

---
+++ src/ts/app.ts
@@ -0,0 +1,31 @@
+const section0 = document.getElementById("section0");
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById('section3');
+const section4 = document.getElementById('section4');
+
+const c1: string = '#932A2B';
+const c2: string = '#FCBE79';
+const c3: string = '#FFE5BE';
+
+let colors: any = [c1, c2, c3];
+
+let bodyCopy: any = document.querySelectorAll('.contentBlock');
+
+
+for (let i = 0; i < bodyCopy.length; i++) void {
+        return bodyCopy[i].style.color = colors[1]
+        
+}
+
+for (var i = 0; i < 10; i++) {
+    setTimeout(function() { console.log(i); }, 100 * i);
+}
+
+
+
+
+
+
+
+
