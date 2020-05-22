a45318fda1f0999e15247083fa055dd28ab9660b
fernanedez1992Mike <fernandez1992mike@gmail.com>
2020-05-21 23:42:10 -0400

    Basic TypeScript Study

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,26 +1,53 @@
+/* Sections */
 const section0 = document.getElementById("section0");
 const section1 = document.getElementById("section1");
 const section2 = document.getElementById("section2");
-const section3 = document.getElementById('section3');
-const section4 = document.getElementById('section4');
 
+/* Brand Colors */
 const c1: string = '#932A2B';
 const c2: string = '#FCBE79';
 const c3: string = '#FFE5BE';
 
 let colors: any = [c1, c2, c3];
 
-let bodyCopy: any = document.querySelectorAll('.contentBlock');
+/* ContentBlock includes | h2-header,h6-subHeader,p-bodyCopy, h4-price */
+let bodyCopy = document.getElementsByTagName('p');
 
+/* Form Selections */
+let button = document.getElementById('button');
 
-for (let i = 0; i < bodyCopy.length; i++) void {
-        return bodyCopy[i].style.color = colors[1]
-        
+function add(n1:number, n2:number) {
+    return n1+n2;
 }
 
-for (var i = 0; i < 10; i++) {
-    setTimeout(function() { console.log(i); }, 100 * i);
+function printResult(num: number) {
+    console.log('Result: ' + num);
 }
+
+function addAndHandle (n1: number, n2:number, cb: (num: number) => void){
+    const result = n1 + n2;
+    cb(result);
+}
+
+printResult(add(5,12));
+
+let combineValues: (a: number, b:number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10,40,(result)=>{
+    console.log(result);
+});
+
+//generateError('An error occured!', 500);
+
+
+
+
+
+
 
 
 
