7d3b3fca861ff066ff2d659c60a0443cce7868b9
fernanedez1992Mike <fernandez1992mike@gmail.com>
2020-06-19 14:40:09 -0400

    SlideShow Issues

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,8 +4,6 @@ import {HasFormatter} from './interfaces/HasFormmatter';
 import {listTemplate} from './classes/listTemplate';
 
 
-
-
 let docOne: HasFormatter;
 docOne = new invoice ('Yoshi', 'web work', 250);
 let docs: HasFormatter[] = [];
@@ -64,14 +62,147 @@ const greetPerson = (person: isPerson) => {
 
 greetPerson(me);
 
+interface Shape {
+    x: number
+    y: number 
+}
+
+abstract class Shape {
+    public x: number
+    public y: number
+
+    constructor(x: number, y: number){
+        this.x = x
+        this.y = y;
+    }
+    abstract get area(): number
+}
+
+class Square extends Shape {
+    public w: number
+    public h: number
+    constructor(x: number, y: number, w: number, h: number){
+        super(x, y)
+        this.w = w
+        this.h = h
+    }
+    public get area(){
+        return this.w * this.h
+    }
+}
+
+class Circle extends Shape {
+    public r: number 
+    constructor(x: number, y:number, r:number){
+        super(x, y)
+        this.r = r
+    }
+    public get area(){
+        return Math.PI * Math.pow(this.r, 2)
+    }
+}
+
+class Triangle extends Shape {
+    public h: number
+    constructor(x: number, y: number, h:number){
+        super(x, y)
+        this.h = h
+    }
+    public get area(){
+        return this.x *this.y * this.h
+    }
+}
+
+var myCircle = new Circle(0, 0, 5);
+console.log(myCircle.x, myCircle.y, myCircle.area)
+
+var myRect = new Square(2, 5, 10, 30);
+console.log(myRect.x, myRect.y, myRect.area);
+
+var myTri = new Triangle(3, 4, 5);
+console.log(myTri.x, myTri.y, myTri.area);
 
 
+abstract class ContentBlock {
+    public bodyCopy: string;
+    public h1: string;
+    public h6: string;
+
+    constructor(bodyCopy: string, h1:string, h6:string){
+        this.bodyCopy = bodyCopy
+        this.h1 = h1
+        this.h6 = h6        
+    }
+
+    abstract get content(): string    
+}
+
+class Intro extends ContentBlock {
+    public welcome: string;
+
+    constructor(bodyCopy:string, h1:string, welcome:string) {
+        super(bodyCopy, h1, welcome);
+        this.welcome = welcome;
+    }
+
+    public get content(){
+        return this.bodyCopy + this.h1 + this.welcome;
+    }
+}
+
+class Image extends ContentBlock {
+    public img: string;
+
+    constructor(bodyCopy:string, h1:string, img:string){
+        super(bodyCopy, h1, img)
+        this.img = img
+    }
+
+    public get content(){
+        return this.img;
+    }
+}
+
+let newBlock = new Intro('Learning new skills', 'classes are awesome', 'add an image or something!');
+console.log(newBlock.bodyCopy, newBlock.h1, newBlock.h6);
+
+let bioBlock = new Intro('New block for practice', 'Puppies are awesome', 'it is working');
+console.log(bioBlock.bodyCopy, bioBlock.h1, bioBlock.welcome);
+
+let newImg = new Image ('New image', 'Image title', 'image src');
+console.log(newImg.bodyCopy, newImg.h1, newImg.img);
 
 
+let slideIndex:number = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n:number){
+    return showSlides( slideIndex += n );
+}
 
+function currentSlide(n: number){
+    return showSlides( slideIndex = n);
+}
 
+// I do not understand why plusSides and currentSlide is returned as undefined?
 
+function showSlides(n:number):void {
+    let i;
+    let slides: NodeListOf<HTMLElement> = document.querySelectorAll('.wrapperItem');
+    let dots: NodeListOf<HTMLElement> = document.querySelectorAll('.dot');
+
+    if(n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[slideIndex -1].style.display = 'block';
+    dots[slideIndex -1].className += 'active';
+}
 
 
 
